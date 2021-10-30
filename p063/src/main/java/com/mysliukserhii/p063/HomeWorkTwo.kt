e5f49143239ad32f5e4e198299408a1c103f31bd
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2021-10-31 00:18:38 +0300

    FirstTask

---
+++ p063/src/main/java/com/mysliukserhii/p063/HomeWorkTwo.kt
@@ -0,0 +1,11 @@
+package com.mysliukserhii.p063
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeWorkTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_work_two)
+    }
+}
